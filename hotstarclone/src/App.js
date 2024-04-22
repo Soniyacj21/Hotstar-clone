5638dcf9b6b8056ecc1689705b9db314717a9fe3
Soniyacj21 <soniyacj21@gmail.com>
2024-04-22 18:17:43 +0530

    Home page creation

--- hotstarclone/src/App.js
+++ hotstarclone/src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+        <h5> DISNEY + hotstar</h5>
+          <div id="menu-bar">
+            <nav>
+            <ul>
+              <li></li>
+              <li></li>
+
+            </ul>
+      
+    
+            </nav>
+          </div>
+
+
+      </div>
+     
+   
   );
 }
 
